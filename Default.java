5d75f55d93fe1ed509dcff21f329f13bdde828cc
DHARMIREDDI HEMASREE <hemasreeha@gmail.com>
2025-07-28 13:01:01 +0530

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,13 @@
+package Javafundamentals;
+
+public class Default {
+	    int i; 
+		boolean b; 
+		char c; 
+		public static void main(String[] args) { 
+		Default obj = new Default(); 
+		System.out.println("int: " + obj.i); 
+		System.out.println("boolean: " + obj.b); 
+		System.out.println("char: '" + obj.c + "'");  
+		} 
+		} 
---
+++ Operatores.java
@@ -0,0 +1,15 @@
+package Javafundamentals;
+
+public class Operatores {
+		public static void main(String[] args) { 
+		int num = 5; 
+		System.out.println(num++);
+		System.out.println(++num); 
+		int num2 = 8; 
+		System.out.println(num2 >> 1);
+		System.out.println(num2 << 1);  
+		boolean b1 = true, b2 = false; 
+		System.out.println(b1 & b2);  
+		System.out.println(b1 && b2); 
+		} 
+}
